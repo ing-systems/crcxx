f4f023ce5f358a01f56c8e2640a2548406ec78b3
Karol Harasim <karol.harasim@gmail.com>
2023-02-24 12:51:41 +0100

    Make LUT generation const.

--- src/lut.rs
+++ src/lut.rs
@@ -1,37 +1,57 @@
 #[macro_export]
 macro_rules! imp_make_lut_32 {
     ($name: ident, $ty: ty, $reflect_byte: path, $reflect_value: path) => {
-        pub fn $name(poly: $ty, reflect: bool) -> [$ty; 32] {
+        pub const fn $name(poly: $ty, reflect: bool) -> [$ty; 32] {
             const BITS: usize = ::core::mem::size_of::<$ty>() * 8;
             const TOP_BIT: $ty = 1 << (BITS - 1);
 
             let mut table = [0 as $ty; 32];
 
-            for index in 0..16usize {
+            let mut index = 0;
+            while index < 16 {
                 let byte = if reflect { $reflect_byte(index as $ty) } else { index as $ty };
 
                 let mut value: $ty = byte << (BITS - 8);
 
                 // Step through all the bits in the byte
-                for _ in 0..8 {
-                    if (value & TOP_BIT) != 0 { value = (value << 1) ^ poly } else { value <<= 1 }
+                let mut bit = 0;
+                while bit < 8 {
+                    if (value & TOP_BIT) != 0 {
+                        value = (value << 1) ^ poly
+                    } else {
+                        value <<= 1
+                    }
+
+                    bit += 1;
                 }
 
                 table[index] = if reflect { $reflect_value(value) } else { value };
+
+                index += 1;
             }
 
-            for index in 0..16usize {
+            let mut index = 0;
+            while index < 16 {
                 let byte =
                     if reflect { $reflect_byte((index << 4) as $ty) } else { (index << 4) as $ty };
 
                 let mut value: $ty = byte << (BITS - 8);
 
                 // Step through all the bits in the byte
-                for _ in 0..8 {
-                    if (value & TOP_BIT) != 0 { value = (value << 1) ^ poly } else { value <<= 1 }
+                let mut bit = 0;
+                while bit < 8 {
+                    if (value & TOP_BIT) != 0 {
+                        value = (value << 1) ^ poly
+                    } else {
+                        value <<= 1
+                    }
+
+                    bit += 1;
                 }
 
                 table[index + 16] = if reflect { $reflect_value(value) } else { value };
+
+                index += 1;
             }
 
             table
@@ -42,23 +62,33 @@ macro_rules! imp_make_lut_32 {
 #[macro_export]
 macro_rules! imp_make_lut_256 {
     ($name: ident, $ty: ty, $reflect_byte: path, $reflect_value: path) => {
-        pub fn $name(poly: $ty, reflect: bool) -> [$ty; 256] {
+        pub const fn $name(poly: $ty, reflect: bool) -> [$ty; 256] {
             const BITS: usize = ::core::mem::size_of::<$ty>() * 8;
             const TOP_BIT: $ty = 1 << (BITS - 1);
 
             let mut table = [0 as $ty; 256];
 
-            for index in 0..256usize {
+            let mut index = 0;
+            while index < 256 {
                 let byte = if reflect { $reflect_byte(index as $ty) } else { index as $ty };
 
                 let mut value: $ty = byte << (BITS - 8);
 
                 // Step through all the bits in the byte
-                for _ in 0..8 {
-                    if (value & TOP_BIT) != 0 { value = (value << 1) ^ poly } else { value <<= 1 }
+                let mut bit = 0;
+                while bit < 8 {
+                    if (value & TOP_BIT) != 0 {
+                        value = (value << 1) ^ poly
+                    } else {
+                        value <<= 1
+                    };
+
+                    bit += 1;
                 }
 
                 table[index] = if reflect { $reflect_value(value) } else { value };
+
+                index += 1;
             }
 
             table
@@ -69,45 +99,54 @@ macro_rules! imp_make_lut_256 {
 #[macro_export]
 macro_rules! imp_make_sliced_lut {
     ($name: ident, $ty: ty, $make_base_lut_256: path) => {
-        pub fn $name(poly: $ty, reflect: bool) -> [[$ty; 256]; SLICES] {
+        pub const fn $name(poly: $ty, reflect: bool) -> [[$ty; 256]; SLICES] {
             const BITS: usize = ::core::mem::size_of::<$ty>() * 8;
             const SHIFT: usize = if BITS > 8 { 8 } else { 0 };
 
-            let base_lut = $make_base_lut_256(poly, reflect);
-            let mut sliced_lut = [[0 as $ty; 256]; SLICES];
-
-            sliced_lut[0].copy_from_slice(&base_lut);
+            let mut lut = [[0 as $ty; 256]; SLICES];
+            lut[0] = $make_base_lut_256(poly, reflect);
 
             if reflect {
-                for n in 0..256 {
-                    let mut crc = sliced_lut[0][n];
+                let mut n = 0;
+                while n < 256 {
+                    let mut crc = lut[0][n];
 
-                    for k in 1..SLICES {
+                    let mut k = 1;
+                    while k < SLICES {
                         if BITS > 8 {
-                            crc = sliced_lut[0][(crc & 0xff) as usize] ^ (crc >> SHIFT);
+                            crc = lut[0][(crc & 0xff) as usize] ^ (crc >> SHIFT);
                         } else {
-                            crc = sliced_lut[0][(crc & 0xff) as usize];
+                            crc = lut[0][(crc & 0xff) as usize];
                         }
-                        sliced_lut[k][n] = crc;
+                        lut[k][n] = crc;
+
+                        k += 1;
                     }
+
+                    n += 1;
                 }
             } else {
-                for n in 0..256 {
-                    let mut crc = sliced_lut[0][n];
+                let mut n = 0;
+                while n < 256 {
+                    let mut crc = lut[0][n];
 
-                    for k in 1..SLICES {
+                    let mut k = 1;
+                    while k < SLICES {
                         if BITS > 8 {
-                            crc = sliced_lut[0][((crc >> (BITS - 8)) & 0xff) as usize]
-                                ^ (crc << SHIFT);
+                            crc = lut[0][((crc >> (BITS - 8)) & 0xff) as usize] ^ (crc << SHIFT);
                         } else {
-                            crc = sliced_lut[0][((crc >> (BITS - 8)) & 0xff) as usize];
+                            crc = lut[0][((crc >> (BITS - 8)) & 0xff) as usize];
                         }
-                        sliced_lut[k][n] = crc;
+                        lut[k][n] = crc;
+
+                        k += 1;
                     }
+
+                    n += 1;
                 }
             }
 
-            sliced_lut
+            lut
         }
     };
 }
@@ -116,16 +155,19 @@ macro_rules! imp_make_sliced_lut {
 macro_rules! imp_reflect_value {
     ($name: ident, $ty:ty) => {
         #[inline]
-        fn $name(mut value: $ty) -> $ty {
+        const fn $name(mut value: $ty) -> $ty {
             const BITS: usize = ::core::mem::size_of::<$ty>() * 8;
 
             let mut reflection: $ty = 0;
 
-            for i in 0..BITS {
+            let mut bit = 0;
+            while bit < BITS {
                 if (value & 0x01) == 1 {
-                    reflection |= 1 << ((BITS - 1) - i)
+                    reflection |= 1 << ((BITS - 1) - bit);
                 }
                 value >>= 1;
+
+                bit += 1;
             }
 
             reflection
@@ -137,17 +179,20 @@ macro_rules! imp_reflect_value {
 macro_rules! imp_reflect_byte {
     ($name: ident, $ty:ty) => {
         #[inline]
-        fn $name(input: $ty) -> $ty {
+        const fn $name(input: $ty) -> $ty {
             const BITS: usize = 8;
 
             let mut reflection: $ty = 0;
             let mut value = input;
 
-            for i in 0..BITS {
+            let mut bit = 0;
+            while bit < BITS {
                 if (value & 0x01) == 1 {
-                    reflection |= 1 << ((BITS - 1) - i)
+                    reflection |= 1 << ((BITS - 1) - bit);
                 }
                 value >>= 1;
+
+                bit += 1;
             }
 
             reflection
